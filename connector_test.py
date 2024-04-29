e5e622cc905c77021f1744cbf478d90fa08315dd
Arjun0216 <arjunbhardwaj0216@gmail.com>
2024-04-29 00:23:38 -0500

    new_user settings

--- connector_test.py
+++ connector_test.py
@@ -9,8 +9,19 @@ connection = mysql.connector.connect(
 )
 
 cursor = connection.cursor()
-
+current_id = 1
 # ------------- FUNCTIONS -------------------------------------------
+def menu():
+    print("1. Check Balance\n")
+    print("2. Make a Deposit\n")
+    print("3. Withdraw\n")
+
+def returnToMenu():
+    a = input("would you like to return to the main menu? (type y for yes or n for no ): ")
+    if a == "y":
+        print("\n")
+        menu()
+        
 def check_balance(credentials):
     testQuery = 'SELECT balance FROM account_info WHERE password = "' + credentials + '"'
     cursor.execute(testQuery)
@@ -19,18 +30,31 @@ def check_balance(credentials):
         return item
 def make_deposit(amount, credentials):
     current_balance = check_balance(credentials)[0]
-    print(type(current_balance))
     new_balance = current_balance + float(amount)
     testQuery = 'UPDATE account_info SET balance = "' + str(new_balance) + '" WHERE password = "' + credentials + '"'
     cursor.execute(testQuery)
 
     print("Success! Your balance is now $" + str(check_balance(credentials)[0]))
+    returnToMenu()
 
-def employee():
-    password = input("Enter employee password: ")
-    if password == "cheetos":
-        print("ye")
+def withdraw(amount, credentials):
+    current_balance = check_balance(credentials)[0]
+    new_balance = current_balance - float(amount)
+    testQuery = 'UPDATE account_info SET balance = "' + str(new_balance) + '" WHERE password = "' + credentials + '"'
+    cursor.execute(testQuery)
+
+    print("Success! Your balance is now $" + str(check_balance(credentials)[0]))
+    returnToMenu()
+def new_account(id, password, name, balance):
+    testQuery = 'INSERT INTO account_info (id, password, name, balance) VALUES ("' + str(id) + '", "' + str(password) + '", "' + str(name) + '", "' + str(balance) + '");'
+    cursor.execute(testQuery)
+    print(str(check_balance(password)[0]))
+    print("Success! Welcome to Bank of Tomorrow\n")
+    returnToMenu()
+
+
 def customer():
+    
     credentials = input("Please input your password to get started: ")
     testQuery = 'SELECT name FROM account_info WHERE password = "' + credentials + '"'
     def name():
@@ -39,19 +63,28 @@ def customer():
             return item
     
     print("Welcome " + str(name()[0]) + "! How can we help you today?\n")
-    print("1. Check Balance\n")
-    print("2. Make a Deposit\n")
-    print("3. Withdraw\n")
+    menu()
 
     choice = input("enter the number of your choice: ")
     if choice == "1":
-        print("ye")
         print("Your Balance is $" + str(check_balance(credentials)[0]))
+        returnToMenu()
     if choice == "2":
         amount = input("How much would you like to deposit? ")
         make_deposit(amount, credentials)
     if choice == "3":
-        withdraw()
+        amount = input("How much would you like to withdraw? ")
+        if float(amount)> check_balance(credentials)[0]:
+            print("insufficient funds")
+        else: withdraw(amount, credentials)
+def new_customer():
+    global current_id
+    current_id += 1
+    balance = 0
+    name = input("Welcome to Bank of Tomorrow! Let's make a new account. What is your name? ")
+    password = input("Awesome " + name + "! What would you like your password to be? ")
+    new_account(current_id, password, name, balance)
+
 
     
     
@@ -59,14 +92,16 @@ def customer():
 #----------------------------MAIN-----------------------
 
 def main():
-    intro = input("Hello! Welcome to the Bank of Tomorrow's Website! Are you a customer or a database employee? Enter c for cutomer or e for employee: ")
-    if intro == "e":
-        employee()
-    elif intro == "c":
-        customer()
+    print("Hello! Welcome to the Bank of Tomorrow's Website!\n")
+    status = input("Are you a new or returning customer? (type n for new or r for returning): ")
+    if status == "n":
+        new_customer()
     else:
-        print("invalid")
+        customer()
+ 
 main()
+
+
 
 
 
