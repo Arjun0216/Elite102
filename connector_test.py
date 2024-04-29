22d9c8435b422a881fc7799271399ca740c07311
Arjun0216 <arjunbhardwaj0216@gmail.com>
2024-04-29 01:28:05 -0500

    Finished with all functionality

--- connector_test.py
+++ connector_test.py
@@ -12,15 +12,17 @@ cursor = connection.cursor()
 current_id = 1
 # ------------- FUNCTIONS -------------------------------------------
 def menu():
+    print("\n")
     print("1. Check Balance\n")
     print("2. Make a Deposit\n")
     print("3. Withdraw\n")
+    print("4. Close an account\n")
+    print("5. Modify Account\n")
 
 def returnToMenu():
-    a = input("would you like to return to the main menu? (type y for yes or n for no ): ")
-    if a == "y":
-        print("\n")
-        menu()
+    choice = input("Would you like to go back to our homepage? (type y for yes or n for no): ")
+    if choice == "y":
+        main()
         
 def check_balance(credentials):
     testQuery = 'SELECT balance FROM account_info WHERE password = "' + credentials + '"'
@@ -42,13 +44,27 @@ def withdraw(amount, credentials):
     new_balance = current_balance - float(amount)
     testQuery = 'UPDATE account_info SET balance = "' + str(new_balance) + '" WHERE password = "' + credentials + '"'
     cursor.execute(testQuery)
-
-    print("Success! Your balance is now $" + str(check_balance(credentials)[0]))
+    print("You have succesfully withrawed $" + amount + "!\n")
     returnToMenu()
+def delete_account(credentials):
+    testQuery = 'DELETE FROM account_info WHERE password = "' + credentials + '"'
+    cursor.execute(testQuery)
+    print("Your account has been deleted. Sorry to see you go.\n")
+    returnToMenu()
+def modify_password(new_val, credentials):
+    testQuery = 'UPDATE account_info SET password = "' + str(new_val) + '" WHERE password = "' + credentials + '"'
+    cursor.execute(testQuery)
+    print("Your password has been changed!\n")
+    returnToMenu()
+def modify_name(new_val, credentials):
+    testQuery = 'UPDATE account_info SET name = "' + str(new_val) + '" WHERE password = "' + credentials + '"'
+    cursor.execute(testQuery)
+    print("Your name has been changed!\n")
+    returnToMenu()
+    
 def new_account(id, password, name, balance):
     testQuery = 'INSERT INTO account_info (id, password, name, balance) VALUES ("' + str(id) + '", "' + str(password) + '", "' + str(name) + '", "' + str(balance) + '");'
     cursor.execute(testQuery)
-    print(str(check_balance(password)[0]))
     print("Success! Welcome to Bank of Tomorrow\n")
     returnToMenu()
 
@@ -77,6 +93,23 @@ def customer():
         if float(amount)> check_balance(credentials)[0]:
             print("insufficient funds")
         else: withdraw(amount, credentials)
+    if choice == "4":
+        delete_account(credentials)
+    if choice == "5":
+        col = "something"
+        print("What would you like to modify? \n")
+        print("1. password\n")
+        print("2. name\n")
+
+        choice = input("Enter the number of your choice here : ")
+        if choice == "1":
+            new_val = input("What would you like to change your password to? ")
+            modify_password(new_val, credentials)
+        else:
+            new_val = input("What would you like to change your name to? ")
+            modify_name(new_val, credentials)
+        
+
 def new_customer():
     global current_id
     current_id += 1
@@ -100,6 +133,7 @@ def main():
         customer()
  
 main()
+
 
 
 
